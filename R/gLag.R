7fc0db11c5deeb8049368c54b8f539a1a847dcdc
Genaro Sucarrat <genaro.sucarrat@bi.no>
2014-10-22 00:00:00 +0000

    version 1.3

--- R/gLag.R
+++ R/gLag.R
@@ -1,10 +1,14 @@
 gLag <-
 function(y, k=1, na.value=NA)
 {
-out <- c(rep(na.value,k),as.vector(y[1:I(length(y)-k)]))
-if(is.zoo(y)){
-  y.index <- index(y)
-  out <- zoo(out, order.by=y.index)
-}
-return(out)
+#OLD code:
+#out <- c(rep(na.value,k),as.vector(y[1:I(length(y)-k)]))
+#if(is.zoo(y)){
+#  y.index <- index(y)
+#  out <- zoo(out, order.by=y.index)
+#}
+#return(out)
+
+  glag(y, k=k, pad.value=na.value, pad=TRUE)
+
 }
